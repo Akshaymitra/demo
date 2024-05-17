0fb55b37d0bdbeeabae3ac265c5723716b0f6f54
Akshaymitra <akshay.m@iosense.io>
2024-05-17 18:50:59 +0530

    new commit

--- demo1/demo1.py
+++ demo1/demo1.py
@@ -1,5 +1,5 @@
 import time
 
 while True:
-    print("Hello, every 4 assssadaasassascazccccncnxbvncxvbnccccccacbcbcbbccccasadsadccccasdas!")
+    print("Hello, every 4 assssadaasassascazadsadaxcxcccccncnxbvncxvbnccccccacbcbcbbccccasadsadccccasdas!")
     time.sleep(5)
--- demo2/demo2.py
+++ demo2/demo2.py
@@ -1,5 +1,5 @@
 import time
 
 while True:
-    print("Hello, every 4 assssadaasassas;;;;;;;;;;;;cazccccccccccacbcbcbbccccasadsadccccasdas!")
+    print("Hello, every 4 assssadaasassas;;;;;;;;;;;;cazccccccccccaaasdasdcbcbcbbccccasadsadccccasdas!")
     time.sleep(5)
